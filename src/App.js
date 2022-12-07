eadb77981c997a89a755878a5d401f463ebce368
unknown <abhijithabi22102001@gmail.com>
2022-12-07 16:18:53 +0530

    counter

--- src/App.js
+++ src/App.js
@@ -1,22 +1,71 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';   //usestate-hook in function
 
 function App() {
+  const[counter,setcounter]=useState(0);  
+  //counter -state -to hold values  , setcounter-values set
+
+  function incrementcounter(){
+  setcounter(counter+1);
+  }
+  
+  function decrementcounter(){
+  if(counter!==0){   //not equalto Zero
+  setcounter(counter-1);
+  }
+  }
+
+  function reset(){
+  setcounter(0)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+     <div id='a1'
+      style={{
+      display:'flex',
+      flexdiretion:'column',
+      alignItems:'center',
+      justifyContent:'center',
+      border:'5px solid black',
+      borderRadius:'20px',
+      width:'40%',
+      height:'300px',
+      backgroundColor:'orange',
+      marginLeft:'400px',
+      marginTop:'100px'
+    }}
+        className="App">
+        <header className="App-header">
+        <h2>Counter Application</h2>
+        <h2>{counter}</h2>
+        <button
+        style={
+          {
+            backgroundColor:"blue",
+            color:"black",
+            padding:"10px",
+          }
+        }
+         onClick={incrementcounter}>ADD</button>
+         <button
+          style={
+           {
+             backgroundColor:"red",
+             color:"black",
+             padding:"10px"
+           }
+         }
+         onClick={decrementcounter}>REDUCE</button>
+        <button
+          style={
+          {
+            backgroundColor:'green',
+            color:'black',
+            padding:'10px'
+          }
+          }
+         onClick={reset}>RESET</button>
+
       </header>
     </div>
   );
---
+++ src/components/Counterfunction.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function counterfunction() {
+  return (
+    <div>counterfunction</div>
+  )
+}
+
+export default counterfunction
